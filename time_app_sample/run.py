b34b9b95f320be51520d36c433c7a597181f874c
Devansh Singh <devansh.singh@Devanshs-MacBook-Pro.local>
2020-10-01 14:03:26 -0400

    final set of changes for matching the time zone

--- time_app_sample/run.py
+++ time_app_sample/run.py
@@ -1,16 +1,12 @@
+
 from flask import Flask
-import datetime
+from datetime import datetime
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
 
 @app.route('/time')
 def time():
-    now = datetime.datetime.now()
-    return now.strftime("%Y-%m-%d %H:%M:%S")
+    now = datetime.now()
+    return str(now)
 
 app.run(host='0.0.0.0',
         port=8080,
